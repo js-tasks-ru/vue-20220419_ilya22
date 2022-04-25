0ddccb084ed38f5356c5c5ce9ef9711bd081e202
mrBizik <bizigg@gmail.com>
2022-04-25 22:18:28 +0500

    Задачи по теме "Основы Vue"

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,36 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const RootComponent = defineComponent({
+  data() {
+    return {
+      operators: {
+        sum(a, b) {
+          return a + b;
+        },
+        subtract(a, b) {
+          return a - b;
+        },
+        multiply(a, b) {
+          return a * b;
+        },
+        divide(a, b) {
+          return a / b;
+        },
+      },
+      operandA: 0,
+      operator: null,
+      operandB: 0,
+    };
+  },
+  computed: {
+    result() {
+      const operatorMethod = this.operators[this.operator];
+      return operatorMethod ? operatorMethod(this.operandA, this.operandB) : null;
+    },
+  },
+});
+
+const app = createApp(RootComponent);
+app.mount('#app');

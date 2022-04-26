7046667234a00b287d145761a830b4059ecaa775
mrBizik <bizigg@gmail.com>
2022-04-26 17:00:53 +0500

    Четвертая задача по теме "Основы Vue"

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,26 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+const RootComponent = defineComponent({
+  data() {
+    return {
+      isLoading: false,
+      meetups: [1, 2, 3, 4, 5],
+      selectedMeetupId: null,
+      meetup: null,
+    };
+  },
+  watch: {
+    async selectedMeetupId(id) {
+      this.meetup = null;
+      if (!id) {
+        return;
+      }
+
+      this.isLoading = true;
+      this.meetup = await fetchMeetupById(id).finally(() => (this.isLoading = false));
+    },
+  },
+});
+const app = createApp(RootComponent);
+app.mount('#app');
